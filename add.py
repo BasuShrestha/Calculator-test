43e5a8f1788eab8b2a354abe01fa0165bc84ac2d
BasuShrestha <106642018+BasuShrestha@users.noreply.github.com>
2022-06-04 21:07:37 +0545

    Added files from a folder
    Added files directly from the folder already created because it was easier.

---
+++ add.py
@@ -0,0 +1,5 @@
+def add_numbers(*args):
+    sum = 0
+    for nums in args:
+        sum += nums
+    return sum
---
+++ multiply.py
@@ -0,0 +1,5 @@
+def multiply_numbers(*args):
+    total_product = 0
+    for nums in args:
+        total *= nums
+    return total_product
